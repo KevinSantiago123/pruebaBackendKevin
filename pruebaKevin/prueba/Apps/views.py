a6bc5f48b0a437581c1452934df493e7c8d75cae
Kevin Santiago <49997245+KevinSantiago123@users.noreply.github.com>
2019-05-30 09:25:01 -0500

    Update views.py

--- pruebaKevin/prueba/Apps/views.py
+++ pruebaKevin/prueba/Apps/views.py
@@ -5,28 +5,29 @@ from django.shortcuts import render, redirect
 from rest_framework.response import Response
 from rest_framework.views import APIView
 import requests
+import json
 
 class UsuarioView(viewsets.ModelViewSet):
 
-	def recorrer(requests):
-		queryset = Usuario.objects.all()
-		for buscar in queryset:
-			if buscar.longitud == 0:
-				queryset_direccion = buscar.direccion
-				queryset_ciudad = buscar.ciudad
-				google_maps_url= 'https://maps.googleapis.com/maps/api/geocode/json?address='+queryset_direccion+','+queryset_ciudad+'&key=AIzaSyAYVHSbEo-Rh1qBeOOk_BKiXns7bzVniyQ'
-				google_maps_json= requests.get(google_maps_url)
-				result=google_maps_json.json()
-				lgt = result.get('lng')
-				lat = result.get('lat')
-				buscar.longitud=lgt
-				buscar.latitud=lat
-		return queryset
+	def recorrer():
+        queryset = Usuario.objects.all()
+        for buscar in queryset:
+            if buscar.longitud == 0:
+                queryset_direccion = buscar.direccion
+                queryset_ciudad = buscar.ciudad
+            if __name__=='__main__':
+                google_maps_url= 'https://maps.googleapis.com/maps/api/geocode/json?address='+queryset_direccion+','+queryset_ciudad+'&key=AIzaSyAYVHSbEo-Rh1qBeOOk_BKiXns7bzVniyQ'
+                google_maps_json= requests.get(google_maps_url)
+                if google_maps_json.status_code==200:
+                    result=google_maps_json.json()
+                    lgt = result ['results'][0]['geometry']['location']['lng']
+                    lat = result ['results'][0]['geometry']['location']['lat']
+                    buscar.longitud=lgt
+                    buscar.latitud=lat
+        return queryset
 
 	queryset = Usuario.objects.all()
 	serializer_class = UsuarioSerializer
-	
-	
 
 
 		
